181708c730a4e8870cbcae19da596451c1bd6792
Sujal Nanavare <sujalnanavare121@gmail.com>
2026-09-17 14:38:43 +0530

    Add array code

--- Array.py
+++ Array.py
@@ -41,11 +41,39 @@
 
 # 1) Built in Function
 
-arr = [10,20,30,40,50]
-pos = 3
-element = 25
+# arr = [10,20,30,40,50]
+# pos = 3
+# element = 25
 
-print("Array Before Insertion")
-print(arr)
+# print("Array Before Insertion")
+# print(arr)
 
-arr.insert(pos-1,element)
+# arr.insert(pos-1,element)
+
+# print("Array after Insertion")
+# print(arr)
+
+# 2) Using Custom Method
+
+# Python program to insert given element at a given position
+# in an array using custom method
+
+if __name__ == "__main__":
+    n = 4
+    arr = [10, 20, 30, 40, 0]
+    ele = 50
+    pos = 2
+    print("Array before insertion")
+    for i in range(n):
+        print(arr[i], end=' ')
+
+    # Shifting elements to the right
+    for i in range(n, pos - 1, -1):
+        arr[i] = arr[i - 1]
+
+    # Insert the new element at index pos - 1
+    arr[pos - 1] = ele
+
+    print("\nArray after insertion")
+    for i in range(n + 1):
+        print(arr[i], end=' ')
